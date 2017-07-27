ad90b349473ad9ec94656c3a1f86599e9bb5256e
Karl Wallace <karlwallace@gmail.com>
2017-07-27 20:19:51 +0000

    Trying to make calls

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+gem 'rest-client', '~> 1.8'
 gem 'json', '~> 1.8', '>= 1.8.3'
 gem 'twilio-ruby', '~> 5.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    root 'pages#home'
+    get  '/home', to: 'pages#home'
+    get  '/message', to: 'pages#sendMessage'
+    get  '/call', to: 'pages#startCall'
+    
+    post 'message', to: 'pages#SMSsend'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
